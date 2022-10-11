723bc7cfc97d416bc71bd425f5222a0619ed35c0
marthydavid <marthy.david@gmail.com>
2022-10-11 16:09:42 +0200

    Fix missing template_file from M1 platform

--- compute/main.tf
+++ compute/main.tf
@@ -40,9 +40,15 @@ resource "vsphere_virtual_machine" "vm1" {
     template_uuid = data.vsphere_virtual_machine.local-template.id
   }
   extra_config = {
-    "guestinfo.userdata"          = "${base64encode(data.template_file.cloud_init_template.rendered)}"
+    "guestinfo.userdata"          = "${base64encode(templatefile("${path.module}/templates/userdata.tpl",{}))}"
     "guestinfo.userdata.encoding" = "base64"
-    "guestinfo.metadata"          = "${base64encode(data.template_file.metadata1.rendered)}"
+    "guestinfo.metadata"          = "${base64encode(templatefile("${path.module}/templates/metadata.tpl",
+                                      {
+                                            instance_name = "${local.node.prefix}01.${local.node.domain}"
+                                            ip_addr = "${local.node.ips[0]}/24"
+                                            def_gw = "192.168.10.254"
+                                      }
+    ))}"
     "guestinfo.metadata.encoding" = "base64"
   }
 }
@@ -83,9 +89,16 @@ resource "vsphere_virtual_machine" "vm2" {
     template_uuid = data.vsphere_virtual_machine.local-template.id
   }
   extra_config = {
-    "guestinfo.userdata"          = "${base64encode(data.template_file.cloud_init_template.rendered)}"
+        "guestinfo.userdata"          = "${base64encode(templatefile("${path.module}/templates/userdata.tpl",{}))}"
     "guestinfo.userdata.encoding" = "base64"
-    "guestinfo.metadata"          = "${base64encode(data.template_file.metadata2.rendered)}"
+    "guestinfo.metadata"          = "${base64encode(templatefile("${path.module}/templates/metadata.tpl",
+                                      {
+                                            instance_name = "${local.node.prefix}02.${local.node.domain}"
+                                            ip_addr = "${local.node.ips[1]}/24"
+                                            def_gw = "192.168.10.254"
+                                      }
+    ))}"
+    "guestinfo.metadata.encoding" = "base64"
     "guestinfo.metadata.encoding" = "base64"
   }
 }
@@ -128,53 +141,15 @@ resource "vsphere_virtual_machine" "vm3" {
 
 
   extra_config = {
-    "guestinfo.userdata"          = "${base64encode(data.template_file.cloud_init_template.rendered)}"
+    "guestinfo.userdata"          = "${base64encode(templatefile("${path.module}/templates/userdata.tpl",{}))}"
     "guestinfo.userdata.encoding" = "base64"
-    "guestinfo.metadata"          = "${base64encode(data.template_file.metadata3.rendered)}"
-    "guestinfo.metadata.encoding" = "base64"
-  }
-}
-resource "vsphere_virtual_machine" "vm4" {
-  name             = "lxd"
-  folder           = resource.vsphere_folder.folder.path
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore1.id
-
-  num_cpus = var.cpu
-  memory   = var.memory
-
-  enable_disk_uuid = "true"
-
-  firmware = var.firmware
-  guest_id = "rhel8_64Guest"
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-
-  }
-  wait_for_guest_net_timeout = 0
-
-  disk {
-    label            = "disk0"
-    unit_number      = 0
-    size             = var.root_disk_size
-    thin_provisioned = true
-  }
-  disk {
-    label            = "disk1"
-    unit_number      = 1
-    size             = var.zfs_disk_size
-  }
-  clone {
-    #template_uuid = data.vsphere_content_library_item.library-template.id
-    template_uuid = data.vsphere_virtual_machine.local-template.id
-  }
-
-
-  extra_config = {
-    "guestinfo.userdata"          = "${base64encode(data.template_file.cloud_init_template.rendered)}"
-    "guestinfo.userdata.encoding" = "base64"
-    "guestinfo.metadata"          = "${base64encode(data.template_file.metadata4.rendered)}"
+    "guestinfo.metadata"          = "${base64encode(templatefile("${path.module}/templates/metadata.tpl",
+                                      {
+                                            instance_name = "${local.node.prefix}03.${local.node.domain}"
+                                            ip_addr = "${local.node.ips[2]}/24"
+                                            def_gw = "192.168.10.254"
+                                      }
+    ))}"
     "guestinfo.metadata.encoding" = "base64"
   }
 }
